3c59d504ba0a8fd9a724ba732bb9512b4afcb2e0
Georgy <104967130+dmagog@users.noreply.github.com>
2025-03-31 16:13:40 +0500

    Add files via upload

---
+++ database/database.py
@@ -0,0 +1,21 @@
+from sqlmodel import SQLModel, Session, create_engine 
+from contextlib import contextmanager
+from .config import get_settings
+from services.crud.demo import *
+
+engine = create_engine(url=get_settings().DATABASE_URL_psycopg, 
+                       echo=True, pool_size=5, max_overflow=10)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+        
+def init_db(demostart = None):
+    SQLModel.metadata.drop_all(engine)
+    SQLModel.metadata.create_all(engine)
+
+    #Если инициализация базы происходит с созданием демо параметров
+    if demostart:
+        create_demo_user(Session(engine)) # Создадим демо юзеров
+        create_demo_OperationsList(Session(engine)) # Прогоним случайные операции оплаты-пополнения
+        create_demo_model(Session(engine))  #создадим тестовые модели
---
+++ services/crud/historyOperation.py
@@ -0,0 +1,12 @@
+from sqlmodel import SQLModel, Field 
+from typing import Optional, List
+from models.HistoryOperation import *
+
+
+# Операция пополнения балланса    
+def update_HistoryOperationsList(new_operation: HistoryOperation, session) -> bool:
+    session.add(new_operation)  # создали новую запись в истории операций
+    session.commit() 
+    session.refresh(new_operation)
+
+    return True
